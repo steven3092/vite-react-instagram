9e9e5e94388449de0dbfb25be2b3495237588c60
steven fauterre <steven.fauterre@wefox.com>
2024-07-09 15:44:32 +0200

    change prop icon

--- src/assets/message-circle-icon.tsx
+++ src/assets/message-circle-icon.tsx
@@ -18,9 +18,9 @@ export const MessageCircleIcon = ({
       <path
         d="M15.6953 19.2318L19.1027 20.3676C19.8845 20.6282 20.6282 19.8844 20.3676 19.1027L19.2318 15.6953M15.3 19.1C15.3 19.1 14.0847 20 11.5 20C6.80558 20 3 16.1944 3 11.5C3 6.80558 6.80558 3 11.5 3C16.1944 3 20 6.80558 20 11.5C20 14 19.1 15.3 19.1 15.3"
         stroke={color}
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
